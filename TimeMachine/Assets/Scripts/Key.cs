1e3a877645cbc425dac320c8685d69fdb52eee53
Sam Bian <124806197+TantaiYanhen@users.noreply.github.com>
2025-01-22 10:57:47 -0800

    level1

---
+++ TimeMachine/Assets/Scripts/Key.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private bool isPickUp = false;
+    private GameObject player;
+    private SpriteRenderer playerRenderer;
+    private SpriteRenderer keyRenderer;
+    [SerializeField] private float offset = 0.7f;
+    private void Start() {
+        keyRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if(isPickUp){
+            followPlayer();
+        }
+    }
+
+    private void followPlayer(){
+        keyRenderer.flipY = playerRenderer.flipX;
+        Vector3 playerPosition = player.transform.position;
+        transform.position = new Vector3(playerPosition.x + (playerRenderer.flipX?-offset:offset), playerPosition.y,playerPosition.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            player = other.gameObject;
+            isPickUp = true;
+            playerRenderer = other.gameObject.GetComponent<SpriteRenderer>();
+        }
+    }
+}
