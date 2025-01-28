e2927c5d4a8ca177791c1696335f4171ee3a9062
Montgomery Forde <montgomery@ucsb.edu>
2025-01-27 22:38:47 -0800

    more object controller fixes

--- TimeMachine/Assets/Scripts/ObjectTimeController.cs
+++ TimeMachine/Assets/Scripts/ObjectTimeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ObjectTimeController : MonoBehaviour
 {
@@ -13,18 +14,16 @@ public class ObjectTimeController : MonoBehaviour
     private SpriteRenderer sprite;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
-        if(gameObject.GetComponent<Animator>() != null){
-            anim = GetComponent<Animator>();
-        }
+        anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update(){
         if(Input.GetKey(KeyCode.E)){
-            rb.isKinematic = true;
+            if(rb) rb.isKinematic = true;
             rewinding = true; // TODO: implement event manager to tell other things to stop moving/doing their thing while we are rewinding
         } else{
-            rb.isKinematic = false;
+            if(rb) rb.isKinematic = false;
             rewinding = false;
         }
     }
@@ -41,8 +40,12 @@ public class ObjectTimeController : MonoBehaviour
         //while(rewindFrames.Count * Time.fixedDeltaTime >= rewindTime){ // while in case fixed intervals turn out to be a bad idea, then i can easily swap to normal timeupdates
         //    rewindFrames.RemoveFirst();
         //}
-        TimeInfo timeInfo = new TimeInfo(transform.position, rb.velocity);
-        timeInfo.setFlip(sprite.flipX);
+        Vector2 velocity;
+        if(rb) velocity = rb.velocity;
+        else velocity = new Vector2(0,0);
+        
+        TimeInfo timeInfo = new TimeInfo(transform.position, velocity);
+        if(sprite) timeInfo.setFlip(sprite.flipX);
         timeInfo.setRotation(transform.rotation);
         if(anim != null){
             timeInfo.setAnim(anim.GetInteger("state"));
@@ -53,15 +56,15 @@ public class ObjectTimeController : MonoBehaviour
     void RewindStoredSeconds(){
         if(rewindFrames.Count > 0){
             transform.position = rewindFrames.Last.Value.GetPosition();
-            rb.velocity = rewindFrames.Last.Value.GetVelocity();
+            if(rb) rb.velocity = rewindFrames.Last.Value.GetVelocity();
             if(anim != null){
                 anim.SetInteger("state", rewindFrames.Last.Value.GetAnimState());
             }
-            sprite.flipX = rewindFrames.Last.Value.GetSpriteFlip();
+            if(sprite) sprite.flipX = rewindFrames.Last.Value.GetSpriteFlip();
             transform.rotation = rewindFrames.Last.Value.GetRotation();
             rewindFrames.RemoveLast();
         } else{
-            rb.isKinematic = false;
+            if(rb) rb.isKinematic = false;
             rewinding = false;
         }
     }
