a631c026cc6ccb40e8ef52635d7397998b0b83d2
Montgomery Forde <montgomery@ucsb.edu>
2025-02-19 14:33:29 -0800

    removed unncessary garbage unity keeps throwing into my thing

--- TimeMachine/Assets/Scripts/TimeScripts/TimeController.cs
+++ TimeMachine/Assets/Scripts/TimeScripts/TimeController.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections;
+
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Image = UnityEngine.UI.Image;
-using UnityEngine.UIElements;
 
 public class TimeController : MonoBehaviour
 {
