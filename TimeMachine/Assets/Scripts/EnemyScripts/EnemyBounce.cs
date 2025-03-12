8717885d1b0eb6d99d415e8702d3b80dcef81cf8
Mike Petrus <Petrus@ucsb.edu>
2025-03-11 17:02:40 -0700

    Bat prefab + animation, bounceForce Script

---
+++ TimeMachine/Assets/Scripts/EnemyScripts/EnemyBounce.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBounce : MonoBehaviour
+{
+    public SPM playerMovement;
+    public float bounceForce;
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Feet"){
+            playerMovement.bounceForce = bounceForce;
+        }
+    }
+}
